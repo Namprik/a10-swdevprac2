fbc53aaef7a8ff4fef4418c0edbe6623b24b7354
Namprik <wareesa1847@gmail.com>
2024-10-16 07:34:59 +0700

    assignment10

--- src/app/booking/page.tsx
+++ src/app/booking/page.tsx
@@ -1,9 +1,40 @@
 import DateReserve from "@/components/DateReserve";
 import { Select, MenuItem, TextField, Button } from "@mui/material";
-
-export default function Booking() {
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/api/auth/[...nextauth]/authOptions";
+import getUserProfile from "@/libs/getUserProfile";
+export default async function Booking() {
+  const session = await getServerSession(authOptions);
+  if (!session || !session.user.token) return null;
+  const profile = await getUserProfile(session.user.token);
+  var createdAt = new Date(profile.data.createdAt);
   return (
-    <main className="h-full flex justify-center items-center">
+    <main className="h-full flex flex-col justify-center items-center space-y-9">
+      <div>
+        <div className="font-semibold text-center">User Profile</div>
+        <table>
+          <tr>
+            <td>Name:</td>
+            <td>{profile.data.name}</td>
+          </tr>
+          <tr>
+            <td>Email:</td>
+            <td>{profile.data.email}</td>
+          </tr>
+          <tr>
+            <td>Tel.:</td>
+            <td>{profile.data.tel}</td>
+          </tr>
+          <tr>
+            <td>Email:</td>
+            <td>{profile.data.email}</td>
+          </tr>
+          <tr>
+            <td>Member Since:</td>
+            <td>{createdAt.toString()}</td>
+          </tr>
+        </table>
+      </div>
       <form className="flex flex-col w-1/2 p-3 border border-black rounded-xl space-y-4">
         <div className="text-center">Vaccine Booking</div>
         <TextField
